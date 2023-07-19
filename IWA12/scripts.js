1f7a840941994fac7a457469a4c8e889692217f9
Lauren de Blocq <lauren.deblocq@gmail.com>
2023-07-19 12:57:13 +0200

    Finished IWA12

--- IWA12/scripts.js
+++ IWA12/scripts.js
@@ -27,16 +27,18 @@ const STATUS_MAP = {
 
 // Edit below line 
 
-const book1 = document.querySelector('#book1')
-const book1_status = document.querySelector('#book1 span')
-const book1_reserve = document.querySelector('#book1 .reserve')
-const book1_checkOut = document.querySelector('#book1 .checkout')
-const book1_checkIn = document.querySelector('#book1 .checkin')
+/* -----    VARIABLE CREATION/SET UP    ----- */
 
 // status = selector(status)
 // reserve = selector(reserve)
 // checkout = selector(checkout)
 // checkin = selector(checkin)
+
+const book1 = document.querySelector('#book1')
+const book1_status = document.querySelector('#book1 span')
+const book1_reserve = document.querySelector('#book1 .reserve')
+const book1_checkOut = document.querySelector('#book1 .checkout')
+const book1_checkIn = document.querySelector('#book1 .checkin')
 
 const book2 = document.querySelector('#book2')
 const book2_status = document.querySelector('#book2 span')
@@ -44,21 +46,33 @@ const book2_reserve = document.querySelector('#book2 .reserve')
 const book2_checkOut = document.querySelector('#book2 .checkout')
 const book2_checkIn = document.querySelector('#book2 .checkin')
 
-// status = selector(status)
-// reserve = selector(reserve)
-// checkout = selector(checkout)
-// checkin = selector(checkin)
-
 const book3 = document.querySelector('#book3')
 const book3_status = document.querySelector('#book3 span')
 const book3_reserve = document.querySelector('#book3 .reserve')
 const book3_checkOut = document.querySelector('#book3 .checkout')
 const book3_checkIn = document.querySelector('#book3 .checkin')
 
-// status = selector(status)
-// reserve = selector(reserve)
-// checkout = selector(checkout)
-// checkin = selector(checkin)
+
+/* -----     ACTUAL CHALLENGE   ----- */
+
+const changeStatusColor = (element) => {
+    element.style.color = STATUS_MAP[element.innerHTML].color
+}
+
+document.querySelectorAll('span.status').forEach(changeStatusColor)
+book1_status.style.color = STATUS_MAP[book1_status.innerHTML].color
+
+document.querySelectorAll('body div').forEach(book=>{
+    const status = book.querySelector('span.status').innerHTML
+    book.querySelector('.reserve').disabled = !STATUS_MAP[status].canReserve
+    book.querySelector('.checkout').disabled = !STATUS_MAP[status].canCheckout
+    book.querySelector('.checkin').disabled = !STATUS_MAP[status].canCheckIn
+
+})
+
+document.querySelectorAll('button').forEach(element => {
+    element.style.color = 'black'
+})
 
 // status.0.style.color = STATUS_MAP.status.color
 // reserve.0 = STATUS_MAP.status.canReserve ? 'enabled' : 'disabled'
